f2ca28845e582717277c5e016fefaeca44692bd6
azi-prac <azimemory@gmail.com>
2025-05-27 12:50:15 +0900

    lec: kotlin day 2 4

--- a_kotlin/src/main/kotlin/b_operator/b_overload.kt
+++ a_kotlin/src/main/kotlin/b_operator/b_overload.kt
@@ -1,2 +1,15 @@
 package b_operator
 
+data class User(
+    val email:String,
+    val password:String
+){
+    operator fun get(key: String):Any?{
+        return User::class.members.find{it.name == key}?.call(this)
+    }
+}
+
+fun main(){
+    val user = User("hmd@grepp.com", "1234")
+    println(user["email"])
+}
